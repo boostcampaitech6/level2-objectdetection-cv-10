3c88e8e6c41c6b9b10981a43fe14569cf7563556
yumin <edwin.minlee@gmail.com>
2024-01-08 02:59:50 +0900

    Add cascade_train_focal

--- develop/yumin/mmdetection/configs/_base_/schedules/schedule_custom.py
+++ develop/yumin/mmdetection/configs/_base_/schedules/schedule_custom.py
@@ -1,6 +1,6 @@
 # optimizer
 #optimizer = dict(type='AdamW', lr=0.001, weight_decay=0.0001)
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
@@ -8,5 +8,5 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    step=[30, 40])
+    step=[10, 13])
 runner = dict(type='EpochBasedRunner', max_epochs=15)

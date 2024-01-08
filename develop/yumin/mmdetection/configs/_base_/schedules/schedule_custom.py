bb287e9dc2692db970b20b658b52d66266659c49
yumin <edwin.minlee@gmail.com>
2024-01-09 00:16:25 +0900

    Add cascade_train module

--- develop/yumin/mmdetection/configs/_base_/schedules/schedule_custom.py
+++ develop/yumin/mmdetection/configs/_base_/schedules/schedule_custom.py
@@ -1,6 +1,6 @@
 # optimizer
-#optimizer = dict(type='AdamW', lr=0.001, weight_decay=0.0001)
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='AdamW', lr=0.0001, weight_decay=0.0001)
+#optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
@@ -9,4 +9,4 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[10, 13])
-runner = dict(type='EpochBasedRunner', max_epochs=15)
+runner = dict(type='EpochBasedRunner', max_epochs=50)

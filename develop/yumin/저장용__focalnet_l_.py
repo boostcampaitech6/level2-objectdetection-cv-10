f5492114eaf963a6b685d524f4b69c6786b4de20
yumin <edwin.minlee@gmail.com>
2024-01-16 19:49:32 +0900

    Update focalnet

--- develop/yumin/저장용__focalnet_l_.py
+++ develop/yumin/저장용__focalnet_l_.py
@@ -184,7 +184,7 @@ evaluation = dict(interval=1, metric='bbox')
 ##### optimizer #####
 
 optimizer = dict(type='AdamW', lr=0.00025, weight_decay=0.0001) # for focalnet
-optimizer_config = dict(max_norm=5, norm_type=2)
+optimizer_config = dict(grad_clip=dict(max_norm=5, norm_type=2))
 
 
 ##### scheduler #####
@@ -201,5 +201,36 @@ lr_config = dict(
 
 checkpoint_config = dict(max_keep_ckpts=3, interval=1)
 runner = dict(type='EpochBasedRunner', max_epochs=15)
+
+
+
+##### default_runtime #####
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+
+# disable opencv multithreading to avoid system being overloaded
+opencv_num_threads = 0
+# set multi-process start method as `fork` to speed up the training
+mp_start_method = 'fork'
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=16)
 
 
